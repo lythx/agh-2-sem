6c505e08197e729eaccc232588d65ee3c93e7839
lythx <lythxxx@gmail.com>
2024-06-27 13:50:49 +0200

    zadanie drzewa

--- asd/2020/2/zad3.py
+++ asd/2020/2/zad3.py
@@ -12,21 +12,25 @@ class Node:
         self.lazy_right = 0
 
     def __repr__(self):
-        return f'({self.a}, {self.b}) <r:{self.red}, g:{self.green}, b:{self.blue}>'
+        return f'({self.a}, {self.b}) <r:{self.red}, g:{self.green}, b:{self.blue}>, LAZY: ({self.lazy_left}, {self.lazy_right})'
 
 
 def left(i): return 2 * i + 1
 def right(i): return 2 * i + 2
 
 
-def tree_create(tree, i):
+def tree_create(tree, i, dummy):
     if tree[i] is not None:
-        tree[i].green = 1
+        tree[i].green = tree[i].b - tree[i].a + 1
+        if tree[i].b == dummy:
+            tree[i].green -= 1
         return tree[i].a, tree[i].b
-    a, _ = tree_create(tree, left(i))
-    _, b = tree_create(tree, right(i))
+    a, _ = tree_create(tree, left(i), dummy)
+    _, b = tree_create(tree, right(i), dummy)
     tree[i] = Node(a, b)
     tree[i].green = b - a + 1
+    if tree[i].b == dummy:
+        tree[i].green -= 1
     return a, b
 
 
@@ -44,23 +48,33 @@ def flip(p, count):
     p.blue = arr[(count + 1) % 3]
     p.red = arr[(count + 2) % 3]
 
-def tree_query(tree, a, b, i=0, lazy_count=0):
+
+def tree_query(tree, a, b, i=0, lazy_count=0, debug=False):
     p = tree[i]
     inter = intersection(a, b, p.a, p.b)
-    p.lazy_right += lazy_count
-    p.lazy_left += lazy_count
+
     if inter is None:
+        if lazy_count > 0:
+            flip(p, lazy_count)
+            p.lazy_left += lazy_count
+            p.lazy_right += lazy_count
         return
+
+    p.lazy_left += lazy_count
+    p.lazy_right += lazy_count
     a, b = inter
+
     if a == p.a and b == p.b:
         flip(p, lazy_count + 1)
         p.lazy_left += 1
         p.lazy_right += 1
         return
-    tree_query(tree, a, b, left(i), p.lazy_left)
+
+    tree_query(tree, a, b, left(i), p.lazy_left, debug)
     p.lazy_left = 0
-    tree_query(tree, a, b, right(i), p.lazy_right)
+    tree_query(tree, a, b, right(i), p.lazy_right, debug)
     p.lazy_right = 0
+
     l = tree[left(i)]
     r = tree[right(i)]
     p.green = l.green + r.green
@@ -68,7 +82,6 @@ def tree_query(tree, a, b, i=0, lazy_count=0):
     p.blue = l.blue + r.blue
 
 def lamps(n, T):
-    m = len(T)
     points = []
     for a, b in T:
         points.append(Node(a, a))
@@ -76,45 +89,28 @@ def lamps(n, T):
     points.append(Node(0, 0))
     points.append(Node(n - 1, n - 1))
     points.sort(key=lambda x: x.a)
+
     temp = [points[0]]
     for p in points[1:]:
         if p.a != temp[-1].a:
             temp.append(p)
     points = temp
+
     for i in range(1, len(points)):
         if points[i].a - points[i - 1].b > 1:
             points.append(Node(points[i - 1].b + 1, points[i].a - 1))
     points.sort(key=lambda x: x.a)
+
     k = 2**ceil(log2(len(points)))
     points.extend(Node(n, n) for _ in range(k - len(points)))
     tree = [None] * (k - 1)
     tree.extend(points)
-    tree_create(tree, 0)
+    tree_create(tree, 0, n)
 
     mx = 0
-    brute_tab = brute(n , T)
-    i = 0
-    print(tree)
     for a, b in T:
         tree_query(tree, a, b)
         mx = max(mx, tree[0].blue)
-        print(brute_tab[i], mx, a, b, tree)
-        i += 1
-    print(mx)
     return mx
 
-def brute(n, T):
-    arr = [0] * n
-    mx = [-1]
-    for a, b in T:
-        for i in range(a, b + 1):
-            arr[i] = (arr[i] + 1) % 3
-        mx.append(max(mx[-1], arr.count(2)))
-    return mx[1:]
-
-
-# print(lamps(8, [(0, 4), (2, 6)]))
-# runtests( lamps )
-
-lamps(20, [(11, 13), (4, 4), (2, 3), (5, 5), (14, 18), (9, 13), (7, 10), (9, 11), (7, 10), (9, 11), (14, 14), (5, 9), (16, 19), (6, 10), (16, 19), (14, 18), (16, 18), (4, 7), (8, 12), (2, 3)])
-# 13
+runtests( lamps )
